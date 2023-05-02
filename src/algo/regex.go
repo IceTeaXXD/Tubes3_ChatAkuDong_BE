4b9cc69a31674792d4375ee35233092ae3ba2e74
MHEN2606 <matthew.mahendra@gmail.com>
2023-05-02 22:46:07 +0700

    fix: Update regex

--- src/algo/regex.go
+++ src/algo/regex.go
@@ -5,35 +5,46 @@ import (
 	"regexp"
 );
 
-func Regex(flag int, text string) bool{
-	if(flag == 1){
-		/* Periksa apakah terdapat format tanggal dalam teks */
-		regex := regexp.MustCompile(`[0-9]{2}\/[0-9]{2}\/[0-9]{4}`);
-		return_bool, _ := regexp.MatchString("[0-9]{2}/[0-9]{2}/[0-9]{4}", text);
-		if(return_bool){
-			matches := regex.FindAllString(text, -1)
-			fmt.Println(matches[0]);
-			Day(matches[0]);
-		}
-
-		return return_bool;
-
-	}else if(flag == 2){
-		/* Periksa apakah ekspresi matematika yang valid terkandung dalam teks*/
-		regex := regexp.MustCompile(`^\s*([-+]?\s*\d+(\.\d+)?|\(\s*.+?\s*\))\s*(([-+*/]\s*([-+]?\s*\d+(\.\d+)?|\(\s*.+?\s*\)))+)\s*$`);
-		return_bool, _ := regexp.MatchString(`^\s*([-+]?\s*\d+(\.\d+)?|\(\s*.+?\s*\))\s*(([-+*/]\s*([-+]?\s*\d+(\.\d+)?|\(\s*.+?\s*\)))+)\s*$`, text);
-		if(return_bool){
-			matches := regex.FindAllString(text, -1)
-			fmt.Println(matches[0]);
-			res, err := Calculate(matches[0]);
-			if err != nil {
-				fmt.Println("Error:", err)
-			} else {
-				fmt.Printf("Result: %v\n", res)
-			}
-		}
+func Regex(text string) bool{
+	/* Periksa apakah terdapat format tanggal dalam teks */
+	regex := regexp.MustCompile(`([0][1-9]|[1-2][0-9]|[3][0-1])\/(0[1-9]|1[0-2])\/[0-9]{4}`);
+	return_bool, _ := regexp.MatchString(`([0][1-9]|[1-2][0-9]|[3][0-1])\/(0[1-9]|1[0-2])\/[0-9]{4}`, text);
+	if(return_bool){
+		matches := regex.FindAllString(text, -1)
+		fmt.Println(matches[0]);
+		// d := Day(matches[0]);
+		// fmt.Println(d);
 		return return_bool;
 	}
 
-	return true;
+	/* Periksa apakah ekspresi matematika yang valid terkandung dalam teks*/
+	regex = regexp.MustCompile(`^\s*([-+]?\s*\d+(\.\d+)?|\(\s*.+?\s*\))\s*(([-+*/]\s*([-+]?\s*\d+(\.\d+)?|\(\s*.+?\s*\)))+)\s*$`);
+	return_bool, _ = regexp.MatchString(`^\s*([-+]?\s*\d+(\.\d+)?|\(\s*.+?\s*\))\s*(([-+*/]\s*([-+]?\s*\d+(\.\d+)?|\(\s*.+?\s*\)))+)\s*$`, text);
+	if(return_bool){
+		matches := regex.FindAllString(text, -1)
+		fmt.Println(matches[0]);
+		res, _ := Calculate(matches[0])
+		fmt.Println(res);
+		return return_bool;
+	}
+
+	/* Periksa apakah terkandung kata Hapus Pertanyaan xxx */
+	regex = regexp.MustCompile(`(?i)^Hapus Pertanyaan\s+(.*)$`);
+	return_bool, _ = regexp.MatchString(`[Hh][Aa][Pp][Uu][Ss]\s[Pp][Ee][Rr][Tt][Aa][Nn][Yy][Aa][Aa][Nn]\s`, text);
+	if(return_bool){
+		matches := regex.FindStringSubmatch(text)
+		fmt.Println(matches[1])
+		return return_bool;
+	}
+
+	/* Periksa apakah terkandung kata Tambah Pertanyaan XXX dengan jawaban YYY */
+	regex = regexp.MustCompile(`(?i)^tambah pertanyaan\s+(.*?)\s+dengan jawaban\s+(.*?)$`);
+	matches := regex.FindStringSubmatch(text)
+	if(len(matches) > 0){
+		fmt.Println(matches[1])
+		fmt.Println(matches[2])
+		return true;
+	}
+
+	return false;
 }
